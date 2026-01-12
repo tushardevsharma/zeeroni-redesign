cacfcd0fad4a068b20e7fc9b1350f9b76953701a
tushardevsharma <devsharmatushar@gmail.com>
2026-01-12 18:20:50 +0530

    Fix Navigation

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,11 +2,18 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (hash) {
+      const element = document.getElementById(hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 };
